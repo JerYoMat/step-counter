24332c56e5ea97a90e124d378b03decea07a3d75
JerYOMat <jeremy.emata@gmail.com>
2019-03-23 23:45:13 -0400

    basic step counter using redux to manage state;

---
+++ src/StepCounter.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { increment, reset } from './index';
+
+class StepCounter extends React.Component{
+  increment = () => {
+    this.props.increment();
+  }
+
+  reset = () => {
+    this.props.reset();
+  }
+  render() {
+    return (
+      <div className='card'>
+        <div className='step-counter'>
+          <button className='btn btn-primary' onClick={this.increment}>Add Step</button>
+          <button className='btn btn-danger' onClick={this.reset}>Reset</button>
+          <p>Your total number of steps is: {this.props.count} </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    count: state.count
+  }
+}
+
+const mapDispatchToProps = {
+  increment,
+  reset
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(StepCounter);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import StepCounter from './StepCounter';
+import 'bootstrap/dist/css/bootstrap.css';
+
+export const INCREMENT = 'INCREMENT'
+export const RESET = 'RESET'
+
+export function increment() {
+  return { type: INCREMENT };
+}
+
+export function reset() {
+  return { type: RESET }
+}
+
+const initialState = {
+  count: 0
+}
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        count: state.count + 1
+      };
+    case RESET:
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+const App = () => (
+  <Provider store={store}>
+    <StepCounter />
+  </Provider>
+)
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
